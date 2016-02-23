0df24bca9d2e4fbd46a6c08cd4a988d527720275
greater_nemo <avboyd@gmail.com>
2016-02-23 16:04:54 -0600

    Initial content commit
    kilroy.js is basically just a slightly expanded and tweaked pingpong.js
    right now from the examples in the discord.js repo's examples. that'll
    change soon.

---
+++ kilroy.js
@@ -0,0 +1,48 @@
+/*
+	this bot is a ping pong bot, and every time a message
+	beginning with "ping" is sent, it will reply with
+	"pong!".
+*/
+
+var Discord = require("/home/aboyd/node_modules/discord.js");
+
+// Get the email and password
+var AuthDetails = require("./auth.json");
+
+var bot = new Discord.Client();
+
+//when the bot is ready
+bot.on("ready", function () {
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+});
+
+//when the bot disconnects
+bot.on("disconnected", function () {
+	//alert the console
+	console.log("Disconnected!");
+
+	//exit node.js with an error
+	process.exit(1);
+});
+
+//when the bot receives a message
+bot.on("message", function (msg) {
+	//if message begins with "ping"
+	if (msg.content.indexOf("ping") === 0) {
+		//send a message to the channel the ping message was sent in.
+		bot.sendMessage(msg.channel, "pong!");
+
+		//alert the console
+		console.log("pong-ed " + msg.author.username);
+	}
+    else if (msg.content == "(╯°□°）╯︵ ┻━┻") {
+        //unflip the table
+        bot.sendMessage(msg.channel, "┬─┬﻿ ノ( ゜-゜ノ)");
+
+        //log it
+        console.log("unflipped " + msg.author.username);
+    }
+});
+
+bot.login(AuthDetails.email, AuthDetails.password);
+
